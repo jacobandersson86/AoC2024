e8267286afe48ebb233d444db45e12956a25439a
Jacob Andersson <jacob.andersson@plejd.com>
2024-12-05 23:16:21 +0100

    solve day04 part 1

--- day04/ceres_search.py
+++ day04/ceres_search.py
@@ -1,4 +1,5 @@
 import numpy as np
+import re
 
 def read_input(input):
     with open(input) as f:
@@ -17,31 +18,81 @@ def print_text(text):
             print(f"{c} ", end='')
         print('')
 
-def rotate_90(text):
-    # TODO Doesn't rotate, it flips along diagonal (but is this what I want?? )
+def rotate_90_cw(text):
     rows, columns = shape_of_text(text)
     rotated = [['.' for _ in range(rows)] for _ in range(columns)]
 
-    for i, row in enumerate(text):
-        for j, c in enumerate(row):
-            rotated[j][i] = c
+    for row, line in enumerate(text):
+        for column, character in enumerate(line):
+            rotated[column][rows - 1 - row] = character
 
     return rotated
 
-def rotate_45(text):
+def rotate_45_cw(text):
+    rows, columns = shape_of_text(text)
+    output_rows = rows + columns - 1
+    rotated = [['.' for _ in range(columns)] for _ in range(output_rows)]
 
-    start_points = [[]]
+    # Scan Top right triangle
+    lengths = [v + 1 for v in range(columns)]
+    for i, length in enumerate(lengths):
+        for v in range(length):
+            x = v
+            y = length - v - 1
+            c = text[y][x]
+            rotated[i][v] = c
 
+    # Scan bottom left
+    lengths = lengths[-2::-1]
+    for i, length in enumerate(lengths):
+        for v in range(length):
+            x = columns - 1 - (length - v - 1)
+            y = rows - 1 - v
+            c = text[y][x]
+            rotated[i + rows][v] = c
+
+    return rotated
+
+def count_occurrences(text, word):
+    matches = []
+    for line in text:
+        line = ''.join(line)
+        matches.append(re.findall(f"{word}", line))
+    n = [1 for match in matches if len(match) != 0]
+    return sum(n)
 
 def main():
-    normal = read_input('input/example.txt')
-
+    sum = 0
+    normal = read_input('input/input.txt')
     print_text(normal)
+    sum += count_occurrences(normal, 'XMAS')
+    sum += count_occurrences(normal, 'SAMX')
+    print(sum)
 
-    vertical = rotate_90(normal)
+    diagonal_45 = rotate_45_cw(normal)
+    print()
+    print_text(diagonal_45)
+    sum += count_occurrences(diagonal_45, 'XMAS')
+    sum += count_occurrences(diagonal_45, 'SAMX')
+    print(sum)
 
+    vertical = rotate_90_cw(normal)
     print()
     print_text(vertical)
+    sum += count_occurrences(vertical, 'XMAS')
+    sum += count_occurrences(vertical, 'SAMX')
+    print(sum)
+
+    diagonal_135 = rotate_45_cw(vertical)
+    print()
+    print_text(diagonal_135)
+    sum += count_occurrences(diagonal_135, 'XMAS')
+    sum += count_occurrences(diagonal_135, 'SAMX')
+    print(sum)
+
+    print(f"Part 1: {sum}")
+    # 1209 is to low
+
 
 if __name__ == '__main__':
     main()
