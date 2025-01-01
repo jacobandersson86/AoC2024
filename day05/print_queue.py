ccf79bdb74883714f6c60924018ed87fefaa865a
Jacob Andersson <jacob.andersson@plejd.com>
2025-01-01 21:49:06 +0100

    Solve day 05 part 2

--- day05/print_queue.py
+++ day05/print_queue.py
@@ -39,17 +39,33 @@ def has_correct_ordering(rules, queue):
 
     return True
 
+def corrected_queue(rules, queue):
+    for i, page in enumerate(queue):
+        try:
+            after = rules[page]
+        except KeyError:
+            continue
+        before = set(queue[:i])
+        if after.intersection(before):
+            queue.pop(i)
+            queue.insert(i - 1, page)
+            corrected_queue(rules, queue)
+    return queue
+
 def main():
     rules, queues = read_input("day05/input/input.txt")
 
-    sum = 0
+    correct_mids = 0
+    corrected_mids = 0
     for queue in queues:
         if has_correct_ordering(rules, queue) :
-            sum += queue[len(queue) // 2]
+            correct_mids += queue[len(queue) // 2]
+        else:
+            new_queue = [v for v in reversed(corrected_queue(rules, queue))]
+            corrected_mids += new_queue[len(new_queue) // 2]
 
-    print(f"Part 1: {sum}")
-
-
+    print(f"Part 1: {correct_mids}")
+    print(f"Part 2: {corrected_mids}")
 
 if __name__ == '__main__':
     main()
