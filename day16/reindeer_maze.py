006ebf06bd0966e9723ab5d89b6490ca917f4bf1
Jacob Andersson <jacob.andersson@plejd.com>
2025-01-01 16:41:49 +0100

    add solution atempt for day 16 part 2
    Didn't work. I think the problem is that I am counting to little distance in the intersections. Tried a quick fix, but it didn't do it. The real solution is to create the nodes from the actual intersections for part 2.

--- day16/reindeer_maze.py
+++ day16/reindeer_maze.py
@@ -1,6 +1,7 @@
 from position import Position
 from maze import Maze
 import numpy as np
+import copy
 
 directions = [Position(1, 0), Position(0, 1), Position(-1, 0), Position(0, -1)]
 
@@ -64,7 +65,8 @@ def explore_to_next_node(maze, node, dir, nodes):
     nodes = set(nodes)
     pos = node + dir
     # Init to 1 since one step has been taken in the start direction
-    score = 1
+    distance = 1
+    turns = 0
     dead = False
 
     while pos not in nodes or dead:
@@ -79,28 +81,28 @@ def explore_to_next_node(maze, node, dir, nodes):
                     maze[pos + left[dir]]
                 except ValueError:
                     # Dead end. Not an neighbor
-                    return None, 0
+                    return None, 0, 0
                 # Could walk left
                 dir = left[dir]
-                score += 1000
+                turns += 1
                 continue
 
             # Could walk right
             dir = right[dir]
-            score += 1000
+            turns += 1
             continue
 
-        score += 1
+        distance += 1
         pos += dir
 
-    return pos, score
+    return pos, distance, turns
 
 def check_left_right(nodes, pos, dir):
     neighbors = []
     if pos + right[dir] in nodes:
-        neighbors.append((pos + right[dir], 1002))
+        neighbors.append((pos + right[dir], 2, 1))
     if pos + left[dir] in nodes:
-        neighbors.append((pos + left[dir], 1002))
+        neighbors.append((pos + left[dir], 2, 1))
     return neighbors
 
 def find_neighbors(maze: Maze, nodes : list):
@@ -118,24 +120,29 @@ def find_neighbors(maze: Maze, nodes : list):
             if len(close_neighbors) :
                 neighbors.extend(close_neighbors)
 
-            neighbor, score = explore_to_next_node(maze, node, dir, nodes)
+            neighbor, distance, turns = explore_to_next_node(maze, node, dir, nodes)
             if neighbor != None:
                 # Handle turning at the start.
                 if maze[node] == 'S' and dir != Position(1, 0):
-                    score += 1000
-                neighbors.append((neighbor, score))
+                    turns += 1
+                neighbors.append((neighbor, distance, turns))
         neighbor_dict[node] = neighbors
     return neighbor_dict
 
+def calc_score(distance, turns):
+    return distance + turns * 1000
+
 def visit(node, visited : set, neighbors, shortest_path_table):
     node_score, _ = shortest_path_table[node]
-    for neighbor, score in neighbors[node]:
+    for neighbor, distance, turns in neighbors[node]:
+        score = calc_score(distance, turns)
         if neighbor in visited :
             continue
-        last_score, _ = shortest_path_table[neighbor]
+        last_score, nodes = shortest_path_table[neighbor]
         this_score = score + node_score
-        if this_score < last_score:
-            shortest_path_table[neighbor] = (this_score, node)
+        if this_score <= last_score and node not in nodes:
+            nodes.append(node)
+            shortest_path_table[neighbor] = (this_score, nodes)
     visited.add(node)
 
 
@@ -149,13 +156,30 @@ def get_next_to_visit(unvisited, shortest_path_table):
             next_node = node
     return next_node
 
+def find_paths(start_node, end_node, path, shortest_path_table):
+    node = end_node
+    paths = []
+    other_paths = None
+    while node != start_node:
+        _, this_nodes = shortest_path_table[node]
+        for other_node in this_nodes[1:]:
+            new_path = copy.deepcopy(path)
+            new_path.append(other_node)
+            other_paths = find_paths(start_node, other_node, new_path, shortest_path_table)
+        node = this_nodes[0]
+        path.append(node)
+    paths.append(path)
+    if other_paths:
+        paths.extend(other_paths)
+    return paths
+
 def find_shortest_path(nodes, neighbors):
     # Initiate the table
     shortest_path_table = {
-        nodes[0] : (0, None)
+        nodes[0] : (0, [])
     }
     for node in nodes[1:]:
-        shortest_path_table[node] = (np.inf, None)
+        shortest_path_table[node] = (np.inf, [])
 
     unvisited = set(nodes[1:])
     visited = set()
@@ -168,26 +192,41 @@ def find_shortest_path(nodes, neighbors):
             visit(next_node, visited, neighbors, shortest_path_table)
             unvisited.remove(next_node)
 
-    node = nodes[-1]
-    path = [node]
-    while node != nodes[0]:
-        _, node = shortest_path_table[node]
-        path.append(node)
+    start_node = nodes[0]
+    end_node = nodes[-1]
+    path = [end_node]
+    paths = find_paths(start_node, end_node, path, shortest_path_table)
 
     score, _ = shortest_path_table[nodes[-1]]
-    return score, path
+    return score, paths
 
 def main():
-    maze = read_input("day16/input/example0.txt")
+    maze = read_input("day16/input/input.txt")
     maze = Maze(maze)
 
     nodes = find_nodes(maze)
     neighbors = find_neighbors(maze, nodes)
-    score, path = find_shortest_path(nodes, neighbors)
-
-    print(path)
+    score, paths = find_shortest_path(nodes, neighbors)
 
     print(f"Part 1: {score}")
+
+    # This an dirty attempt to fix my problems...
+    total_distance = np.rint(-(len(paths) - 1) * 2.5)
+
+    # Make pairs of all the nodes in the paths
+    pairs = set()
+    for path in paths:
+        for p0, p1 in zip(path, path[1:]):
+            pairs.add((p0, p1))
+
+    for node, neighbor in pairs:
+        data = neighbors[neighbor]
+        distance_to = {position : distance for position, distance, _ in data}
+        total_distance += distance_to[node]
+
+    print(f"Part 2: {total_distance}")
+    # 501 is to low
+
 
 if __name__ == '__main__':
     main()
