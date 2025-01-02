be7499261f80b22dd16c929af7cf84f69c046bd6
Jacob Andersson <jacob.andersson@plejd.com>
2025-01-02 08:32:30 +0100

    Attempt for part 2

--- day16/reindeer_maze.py
+++ day16/reindeer_maze.py
@@ -24,6 +24,43 @@ def read_input(file):
         lines = f.readlines()
     return [[chr for chr in line.strip()] for line in lines]
 
+def find_intersection_nodes(maze : Maze):
+    y_size, x_size = (len(maze.maze), len(maze.maze[0]))
+
+    # Scan for all intersections
+    nodes = []
+    start_node = []
+    end_node = []
+    for y in range(1, y_size - 1):
+        for x in range(1, x_size - 1):
+            pos = Position(x, y)
+            try :
+                item = maze[pos]
+            except ValueError:
+                continue
+
+            # Adding surrounding nodes handles the "turning" and gives a fixed score from node to node.
+            surrounding = []
+            for dir in directions:
+                try :
+                    _ = maze[pos + dir]
+                except ValueError:
+                    continue
+                surrounding.append(pos+dir)
+
+            if len(surrounding) > 2 :
+                nodes.append(pos)
+
+            if item == 'S' :
+                start_node.append(pos)
+                continue
+            if item == 'E' :
+                end_node.append(pos)
+                continue
+
+    return start_node + nodes + end_node
+
+
 def find_nodes(maze: Maze):
     y_size, x_size = (len(maze.maze), len(maze.maze[0]))
 
@@ -60,6 +97,7 @@ def find_nodes(maze: Maze):
                 nodes.extend(surrounding)
 
     return start_node + nodes + end_node
+
 
 def explore_to_next_node(maze, node, dir, nodes):
     nodes = set(nodes)
@@ -210,9 +248,11 @@ def main():
 
     print(f"Part 1: {score}")
 
-    # This an dirty attempt to fix my problems...
-    total_distance = np.rint(-(len(paths) - 1) * 2.5)
+    nodes = find_intersection_nodes(maze)
+    neighbors = find_neighbors(maze, nodes)
+    score, paths = find_shortest_path(nodes, neighbors)
 
+    total_distance = 0
     # Make pairs of all the nodes in the paths
     pairs = set()
     for path in paths:
@@ -226,6 +266,7 @@ def main():
 
     print(f"Part 2: {total_distance}")
     # 501 is to low
+    # 540 is to high
 
 
 if __name__ == '__main__':
