355eb0ef112015a565d1b3613f29eb9e2a069178
Jacob Andersson <jacob.andersson@hotmail.com>
2025-01-09 09:47:26 +0100

    Solve day18

--- day18/ram_run.py
+++ day18/ram_run.py
@@ -45,6 +45,54 @@ def read_input(file):
 
     return ram_bytes
 
+
+def explore_neighbors(ram : RAM):
+    neighbors = dict()
+    for y, row in enumerate(ram.buffer):
+        for x, item in enumerate(row):
+            if item != None:
+                continue
+            node = Position(x, y)
+            node_neighbors = []
+            for dir in [Position(0, 1), Position(1, 0), Position(-1, 0), Position(0, -1)]:
+                neighbor = node + dir
+                try:
+                    item = ram[neighbor]
+                except ValueError:
+                    continue
+                if item == None:
+                    node_neighbors.append(neighbor)
+            neighbors[node] = node_neighbors
+    return neighbors
+
+
+def bfs(start, neighbors, target, ram : RAM):
+    visited = {start}
+    parents = dict()
+    ram[start] = 'O'
+    queue = [start]
+    found = False
+    while not found and len(queue):
+        current_node = queue.pop(0)
+        for node in neighbors[current_node]:
+            if node == target:
+                found = True
+            if node not in visited:
+                queue.append(node)
+                visited.add(node)
+                parents[node] = current_node
+    if not found:
+        return None
+
+    node = target
+    distance = 0
+    while node != start:
+        ram[node] = 'O'
+        node = parents[node]
+        distance += 1
+    return distance
+
+
 def main():
     example = "day18/input/example.txt"
     input = "day18/input/input.txt"
@@ -53,7 +101,7 @@ def main():
     start = Position(0, 0)
     if run == example:
         shape = (7, 7)
-        end = (6, 6)
+        end = Position(6, 6)
         n = 12
     elif run == input:
         shape = (71, 71)
@@ -62,15 +110,23 @@ def main():
 
     ram_bytes = read_input(run)
 
-    ram = RAM(shape)
-    for byte in ram_bytes[:n]:
-        ram[byte] = '#'
+    for i in range(n, len(ram_bytes)) :
+        ram = RAM(shape)
+        for byte in ram_bytes[:i]:
+            ram[byte] = '#'
 
-    ram.show()
+        neighbors = explore_neighbors(ram)
+        distance = bfs(start, neighbors, end, ram)
+        byte = ram_bytes[i - 1]
 
+        if distance == None:
+            ram.show()
+            print(f'Part 2: {byte}')
+            break
 
-
-
+        if i == n:
+            ram.show()
+            print(f"Part 1: {distance}")
 
 
 if __name__ == '__main__':
